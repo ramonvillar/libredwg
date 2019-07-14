9e8acc0e4615e8b656661d066c5066ec7b9cdae6
Reini Urban <rurban@cpan.org>
2019-07-14 20:00:02 +0200

    WIP start obj dat at obj->address, not obj->bitsize_pos
    TODO: adjust other offsets based on the previous dat start
    add dwg_decode_common()

--- include/dwg.h
+++ include/dwg.h
@@ -5857,6 +5857,7 @@ typedef struct _dwg_entity_eed
 typedef struct _dwg_object_entity
 {
   BITCODE_BL objid; /*!< link to the parent */
+  struct _dwg_struct *dwg;
   union
   {
     Dwg_Entity_UNUSED *UNUSED;
@@ -5934,7 +5935,6 @@ typedef struct _dwg_object_entity
     Dwg_Entity_UNKNOWN_ENT *UNKNOWN_ENT;
   } tio;
 
-  struct _dwg_struct *dwg;
   BITCODE_BL num_eed;
   Dwg_Eed *eed; /* see also Dwg_Resbuf* xdata */
 
@@ -6001,6 +6001,7 @@ typedef struct _dwg_object_entity
 typedef struct _dwg_object_object
 {
   BITCODE_BL objid; /*!< link to the parent */
+  struct _dwg_struct *dwg;
   union
   {
     Dwg_Object_BLOCK_CONTROL *BLOCK_CONTROL;
@@ -6120,7 +6121,6 @@ typedef struct _dwg_object_object
     Dwg_Object_UNKNOWN_OBJ *UNKNOWN_OBJ;
   } tio;
 
-  struct _dwg_struct *dwg;
   BITCODE_BL num_eed;
   Dwg_Eed *eed;
 
